88a0177024a05b62bbd9d96dbc35e6dae432ec8a
Mayur Tantwar <mayur25jan@gmail.com>
2019-08-07 15:21:34 +0530

    Version 2 of Buildovo on item click added

--- app/src/test/java/com/InfinitySolutions/buildovo/Testing_class.java
+++ app/src/test/java/com/InfinitySolutions/buildovo/Testing_class.java
@@ -4,35 +4,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Collections;
+
 
 public class Testing_class {
 
 
-    public static void main(String args[]) throws JSONException {
-        System.out.println("Hello World");
-        Testing_class tc=new Testing_class();
-
-        tc.func();
-
-
-
-
+    public static void main(String[] args) throws JSONException {
+        BigInteger bigInteger=new BigInteger("21000254869723212850");
+        System.out.println(bigInteger.isProbablePrime(1));
     }
 
-    public void func() throws JSONException {
-
-        ArrayList<String> stringArrayList=new ArrayList<>();
-
-        stringArrayList.add("Soperd");
-        stringArrayList.add("Alpedo");
-        stringArrayList.add("Cypex");
-        stringArrayList.add("Pexir");
-        stringArrayList.add("Kerops");
-
-
-        System.out.println(stringArrayList);
-
-    }
 }
